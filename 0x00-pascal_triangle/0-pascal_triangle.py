e7452febaca1bf3c61aff1077dab2d40b0c3df14
NegasiAbe <negasiabebe@gmail.com>
2023-04-20 18:17:41 +0200

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     triangle = []
+
     for i in range(n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        if i > 0:
-            row.append(1)
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         triangle.append(row)
+
     return triangle
